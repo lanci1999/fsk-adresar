cbfa8f099b0e4b7fef9f54198a4e387cf31dbf53
lanci1999 <lana.vujicic@fsk.unsa.ba>
2022-01-15 19:21:02 +0100

    Second commit

---
+++ studenti.js
@@ -0,0 +1,40 @@
+function getStudentsFromStorage() {
+    return JSON.parse(sessionStorage.getItem('studenti'))
+}
+
+function setStudentsToStorage(students) {
+    sessionStorage.setItem('studenti', JSON.stringify(students))
+}
+
+
+function dodaj(){
+    var studenti=[];
+    var spaseniStudenti=JSON.parse(sessionStorage.getItem('studenti'))
+    if (spaseniStudenti!= null) studenti=spaseniStudenti
+    var student={
+        firstName:document.getElementById("ime").value,
+        lastName:document.getElementById("prezime").value,
+        number:document.getElementById("index").value,
+        department:document.getElementById("smjer").value,
+        cycle:document.getElementById("ciklus").value,
+        semester:document.getElementById("semestar").value,       
+        }
+    studenti.push(student);
+    console.log(studenti);
+    setStudentsToStorage(studenti);
+}
+
+function trazeni(){
+    let indexTrazenog=document.getElementById("trazeni").value;
+    var trazeniStudent=pretraga(indexTrazenog);
+    if(trazeniStudent===undefined){
+        document.getElementById("ispis").innerHTML="Student sa tim brojem indexa nije unsesn.";
+    }else{
+        document.getElementById("ispis").innerHTML="Traženi student je: "+JSON.stringify(trazeniStudent);
+    }
+    console.log(trazeni);
+}
+
+function pretraga(index){
+    return getStudentsFromStorage().find(student => student.number==index);
+}
